43424d010afd71e54b321ef53e54614b388183f8
FitriWulanSari <79914022+FitriWulanSari@users.noreply.github.com>
2021-03-02 19:20:44 +0700

    Add files via upload

---
+++ Lat2.php
@@ -0,0 +1,9 @@
+<?php
+   //kita akan membuat sebuah variabel dalam php
+   $namadepan = "fitri";
+   $namabelakang ="wulan";
+   $jumlahbarang = 3;
+   $harga = 100000;
+   
+   echo "nama saya adalah $namadepan $namabelakang saya membeli bunga mawar sebanyak $jumlahbarang seharga $harga";
+	?>
